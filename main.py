01e07ed72f914fec87ed1e41469a89b4669346e6
hibestil <halilibrahimbestil@hotmail.com>
2021-02-07 02:37:04 +0300

    Generated random rotated image.

--- main.py
+++ main.py
@@ -3,6 +3,12 @@
 """
 import cv2
 import math
+from feature_matcher import FeatureMatcher
+from random import randrange
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+from matplotlib.pyplot import figure
+from matplotlib.patches import Rectangle
 
 
 def subimage(image, center, theta, width, height):
@@ -43,4 +49,23 @@ def draw_angled_rec(x0, y0, width, height, angle, img, color):
     img = cv2.line(img, pt2, pt3, color, 3)
     img = cv2.line(img, pt3, pt0, color, 3)
     return img
+
+
+
+crop_h = 200
+crop_w = 200
+padding = 200
+
+image_path = "images/StarMap.png"
+base_im = cv2.imread(image_path)
+base_im_h, base_im_w = base_im.shape[:2]
+
+rand_h = randrange(base_im_h - crop_h - padding)
+rand_w = randrange(base_im_w - crop_w - padding)
+rand_theta = randrange(360)
+test_image_start_x = rand_w + padding
+test_image_start_y = rand_h + padding
+
+random_base_coords = (test_image_start_x + crop_w / 2, test_image_start_y + crop_h / 2)
+randomly_rotated_im = subimage(base_im, random_base_coords, rand_theta, crop_w, crop_h)
 

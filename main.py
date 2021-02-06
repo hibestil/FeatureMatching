e9016ff355315a48288434c2d292b4e694ed8aa0
hibestil <halilibrahimbestil@hotmail.com>
2021-02-07 02:36:02 +0300

    Implemented sub_image and draw_angled_rec functions.

--- main.py
+++ main.py
@@ -1,3 +1,46 @@
 """
 @Author : Halil İbrahim Bestil
 """
+import cv2
+import math
+
+
+def subimage(image, center, theta, width, height):
+    '''
+    Rotates OpenCV image around center with angle theta (in deg)
+    then crops the image according to width and height.
+    '''
+
+    # Uncomment for theta in radians
+    # theta *= 180/np.pi
+
+    shape = (image.shape[1], image.shape[0])  # cv2.warpAffine expects shape in (length, height)
+
+    matrix = cv2.getRotationMatrix2D(center=center, angle=theta, scale=1)
+    image = cv2.warpAffine(src=image, M=matrix, dsize=shape)
+
+    x = int(center[0] - width / 2)
+    y = int(center[1] - height / 2)
+
+    image = image[y:y + height, x:x + width]
+
+    return image
+
+
+def draw_angled_rec(x0, y0, width, height, angle, img, color):
+    _angle = angle * math.pi / 180.0
+    b = math.cos(_angle) * 0.5
+    a = math.sin(_angle) * 0.5
+    pt0 = (int(x0 - a * height - b * width),
+           int(y0 + b * height - a * width))
+    pt1 = (int(x0 + a * height - b * width),
+           int(y0 - b * height - a * width))
+    pt2 = (int(2 * x0 - pt0[0]), int(2 * y0 - pt0[1]))
+    pt3 = (int(2 * x0 - pt1[0]), int(2 * y0 - pt1[1]))
+
+    img = cv2.line(img, pt0, pt1, color, 3)
+    img = cv2.line(img, pt1, pt2, color, 3)
+    img = cv2.line(img, pt2, pt3, color, 3)
+    img = cv2.line(img, pt3, pt0, color, 3)
+    return img
+

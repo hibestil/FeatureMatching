d26d42466e5ceaf896728366903b88e410498f3f
hibestil <halilibrahimbestil@hotmail.com>
2021-02-07 04:26:20 +0300

    Added argument parser. Handled exceptions.

--- main.py
+++ main.py
@@ -7,56 +7,90 @@ from feature_matcher import FeatureMatcher
 from random import randrange
 import matplotlib.pyplot as plt
 from matplotlib.pyplot import figure
+import argparse
+
+# Define colors
+red_color = (255, 0, 0,)
+blue_color = (0, 0, 255)
+crop_h = 200
+crop_w = 200
+padding = 200
 
 def main():
-    # Define colors
-    red_color = (255, 0, 0,)
-    blue_color = (0, 0, 255)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-base_image_path", "--base_image_path", type=str,
+                        help="Base image")
+    parser.add_argument("-random_flag", "--random_flag", type=bool, default=False, required=False,
+                        help="Generate random sub images for testing purposes")
+    parser.add_argument("-sub_image_path", "--sub_image_path", type=str,
+                        help="Path of sub-image")
+    args = parser.parse_args()
 
+
+    try:
+        # Init images
+        base_im, sub_image, rand_theta, random_base_coords = init(args)
+
+        # Create figure
+        fig = figure(num=None, figsize=(12, 8), dpi=80, facecolor='w', edgecolor='k')
+        ax = plt.gca()
+
+        # Create a FeatureMatcher Object
+        MIN_MATCH_COUNT = 10
+        matcher = FeatureMatcher(MIN_MATCH_COUNT)
+
+        # Predict coordinates of given sub image in base image
+        result_arr = matcher.match_features(base_im, sub_image)
+        [sub, kp1, scene, kp2, good, matchesMask, estimated_poly_lines] = result_arr
+
+        # Check if match counts enough
+        if estimated_poly_lines is None:
+            raise ValueError("Not enough matches are found - %d/%d" % (len(good), MIN_MATCH_COUNT))
+        else:
+            if args.random_flag:
+                # Plot ground truth of sub-images rectangle
+                scene_gt = draw_angled_rec(random_base_coords[0], random_base_coords[1], crop_w, crop_h, rand_theta,
+                                           base_im, red_color)
+
+            # Plot predicted sub-images coordinates
+            scene_gt_pred = cv2.polylines(scene_gt, estimated_poly_lines, True, blue_color, 3, cv2.LINE_AA)  # Prediction
+
+            # Plot feature matcher related stuff
+            draw_params = dict(matchColor=(0, 255, 0),  # draw matches in green color
+                               singlePointColor=None,
+                               matchesMask=matchesMask,  # draw only inliers
+                               flags=2)
+
+            img3 = cv2.drawMatches(sub, kp1, scene_gt_pred, kp2, good, None, **draw_params)
+
+            # Show result
+            plt.imshow(img3, 'gray'), plt.show()
+
+    except Exception as e:
+        print(e)
+
+
+def init(args):
     # Read the base image
-    image_path = "images/StarMap.png"
-    base_im = cv2.imread(image_path)
+    base_image = cv2.imread(args.base_image_path)
+    rand_theta = None
+    random_base_coords = None
 
-    # Generate random rotated sub image for testing purposes.
-    crop_h = 200
-    crop_w = 200
-    padding = 200
-    randomly_rotated_im, rand_theta, random_base_coords = generate_random_sub_image(base_im, crop_h, crop_w, padding)
-
-    # Create figure
-    fig = figure(num=None, figsize=(12, 8), dpi=80, facecolor='w', edgecolor='k')
-    ax = plt.gca()
-
-    # Create a FeatureMatcher Object
-    MIN_MATCH_COUNT = 10
-    matcher = FeatureMatcher(MIN_MATCH_COUNT)
-
-    # Predict coordinates of given sub image in base image
-    result_arr = matcher.match_features(base_im, randomly_rotated_im)
-    [sub, kp1, scene, kp2, good, matchesMask, estimated_poly_lines] = result_arr
-
-    # Check if match counts enough
-    if estimated_poly_lines is None:
-        print("Not enough matches are found - %d/%d" % (len(good), MIN_MATCH_COUNT))
+    if args.random_flag and (args.sub_image_path is None):
+        # Generate random rotated sub image for testing purposes.
+        randomly_rotated_im, rand_theta, random_base_coords = generate_random_sub_image(base_image, crop_h, crop_w,
+                                                                                        padding)
+        sub_image = randomly_rotated_im
     else:
-        # Plot ground truth of sub-images rectangle
-        scene_gt = draw_angled_rec(random_base_coords[0], random_base_coords[1], crop_w, crop_h, rand_theta, base_im, red_color)
+        if args.sub_image_path:
+            sub_image = cv2.imread(args.sub_image_path)
+        else:
+            raise Exception("Please provide sub-image path.")
 
-        # Plot predicted sub-images coordinates
-        scene_gt_pred = cv2.polylines(scene_gt, estimated_poly_lines, True, blue_color, 3, cv2.LINE_AA)  # Prediction
+    return base_image, sub_image, rand_theta, random_base_coords
 
-        # Plot feature matcher related stuff
-        draw_params = dict(matchColor=(0, 255, 0),  # draw matches in green color
-                           singlePointColor=None,
-                           matchesMask=matchesMask,  # draw only inliers
-                           flags=2)
 
-        img3 = cv2.drawMatches(sub, kp1, scene_gt_pred, kp2, good, None, **draw_params)
-
-        # Show result
-        plt.imshow(img3, 'gray'), plt.show()
-
-def subimage(image, center, theta, width, height):
+def crop_sub_image(image, center, theta, width, height):
     '''
     Rotates OpenCV image around center with angle theta (in deg)
     then crops the image according to width and height.
@@ -106,11 +140,10 @@ def generate_random_sub_image(base_im, crop_h=200, crop_w=200, padding=200):
     test_image_start_y = rand_h + padding
 
     random_base_coords = (test_image_start_x + crop_w / 2, test_image_start_y + crop_h / 2)
-    randomly_rotated_im = subimage(base_im, random_base_coords, rand_theta, crop_w, crop_h)
+    randomly_rotated_im = crop_sub_image(base_im, random_base_coords, rand_theta, crop_w, crop_h)
 
     return randomly_rotated_im, rand_theta, random_base_coords
 
+
 if __name__ == "__main__":
     main()
-
-
